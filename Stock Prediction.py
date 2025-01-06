208a08e4759e3b1eff97d80e396c886ab36f490f
gexjerrygarcia <157856572+gexjerrygarcia@users.noreply.github.com>
2025-01-05 20:33:34 -0500

    Update Stock Prediction.py

--- Stock Prediction.py
+++ Stock Prediction.py
@@ -5,7 +5,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_score
 
 sp500 = yf.Ticker("^GSPC")
-
 sp500 = sp500.history(period = "max")
 
 
@@ -14,27 +13,17 @@ del sp500['Stock Splits']
 
 
 sp500["Tomorrow"] = sp500["Close"].shift(-1)
-
 sp500["Target"] = (sp500["Tomorrow"] > sp500["Close"]).astype(int)
-
-
 sp500 = sp500.loc['1990-01-01':].copy()
-
-
 model = RandomForestClassifier(n_estimators=100,min_samples_split=100,random_state=1)
-
 train = sp500.iloc[:-100]
-
 test = sp500.iloc[-100:]
 
 
 predictors = ["Close","Volume","Open","High","Low"]
-
 model.fit(train[predictors],train["Target"])
 preds = model.predict(test[predictors])
-
 preds = pd.Series(preds,index=test.index)
-
 combined = pd.concat([test["Target"],preds],axis=1)
 
 def predict(train,test,predictors,model):
@@ -45,6 +34,7 @@ def predict(train,test,predictors,model):
     preds = pd.Series(preds,index=test.index,name="Predictions")
     combined = pd.concat([test["Target"],preds],axis=1)
     return combined
+
 def backtest(data,model,predictors,start=2500,step=250):
     all_predictions = []
 
@@ -53,6 +43,7 @@ def backtest(data,model,predictors,start=2500,step=250):
         test = data.iloc[i:(i+step)].copy()
         predictions = predict(train,test,predictors,model)
         all_predictions.append(predictions)
+    
     return pd.concat(all_predictions)
 
 predictions = backtest(sp500,model,predictors)
@@ -70,9 +61,7 @@ for horizon in horizons:
     new_predictors += [ratio_column,trend_column]
 
 sp500 = sp500.dropna()
-
 model = RandomForestClassifier(n_estimators=200,min_samples_split=50,random_state=1)
-
 predictions = backtest(sp500,model,new_predictors)
 
 
